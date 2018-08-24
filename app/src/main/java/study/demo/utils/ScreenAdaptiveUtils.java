3c030cfeb9cefeec92ca84fd5ab3a5188a7c860e
jason <lishuanglongl@aliyun.com>
2018-08-24 08:52:47 +0800

    封装今日头条适配方案（1.0）。

--- app/src/main/java/study/demo/utils/ScreenAdaptiveUtils.java
+++ app/src/main/java/study/demo/utils/ScreenAdaptiveUtils.java
@@ -12,7 +12,7 @@ import study.demo.App;
 /**
  * 根据 今日头条技术团队 分享的android适配方案封装
  *
- * @author jasonl 2018/8/23
+ * @author lishuanglong 2018/8/23
  */
 public class ScreenAdaptiveUtils {
 
@@ -22,39 +22,42 @@ public class ScreenAdaptiveUtils {
     /**
      * 根据设备的高适配
      */
-    public static void screenAdaptiveHeight(@NonNull Context activity) {
-        setCustomDensity(activity, true);
+    public static void screenAdaptiveHeight(@NonNull Context activity, @NonNull Context application, int designSizeDP) {
+        setCustomDensity(activity, application, true, designSizeDP);
     }
 
     /**
      * 根据设备的宽适配
      */
-    public static void screenAdaptiveWidth(@NonNull Context activity) {
-        setCustomDensity(activity, false);
+    public static void screenAdaptiveWidth(@NonNull Context activity, @NonNull Context application, int designSizeDP) {
+        setCustomDensity(activity, application, false, designSizeDP);
     }
 
     /**
      * 关闭，取消屏幕适配
      */
-    private static void cancelAdaptive(@NonNull Context activity) {
-        closeAdaptive(activity);
+    private static void cancelAdaptive(@NonNull Context activity, @NonNull Context application) {
+        closeAdaptive(activity, application);
     }
 
     /**
-     * 以设计图纸宽为360dp，高640dp 密度为3为基准
-     * 修改系统的 Density（密度、比例）、dpi、scaledDensity
+     * @param activity      activity 上下文
+     * @param application   application 上下文
+     * @param isOrientation 为 true 是以高适配，为false以宽适配
+     * @param designSizeDP  以设计图宽(dp)，高(dp)为基准,修改系统的 Density（密度、比例）、dpi、scaledDensity
      */
-    private static void setCustomDensity(@NonNull Context activity, boolean isOrientation) {
-        DisplayMetrics applicationDisplayMetrics = App.getContext().getResources().getDisplayMetrics();
+    private static void setCustomDensity(@NonNull Context activity, @NonNull final Context application, boolean isOrientation,
+                                         int designSizeDP) {
+        DisplayMetrics applicationDisplayMetrics = application.getResources().getDisplayMetrics();
 
         if (sNoncompatDensity == 0) {
             sNoncompatDensity = applicationDisplayMetrics.density;
             sNoncompatScaledDensity = applicationDisplayMetrics.scaledDensity;
-            App.getContext().registerComponentCallbacks(new ComponentCallbacks() {
+            application.registerComponentCallbacks(new ComponentCallbacks() {
                 @Override
                 public void onConfigurationChanged(Configuration newConfig) {
                     if (newConfig != null && newConfig.fontScale > 0) {
-                        sNoncompatScaledDensity = App.getContext().getResources().getDisplayMetrics().scaledDensity;
+                        sNoncompatScaledDensity = application.getResources().getDisplayMetrics().scaledDensity;
                     }
                 }
 
@@ -65,9 +68,9 @@ public class ScreenAdaptiveUtils {
         }
         float targetDensity = 0.0F;
         if (isOrientation) {
-            targetDensity = (float) applicationDisplayMetrics.heightPixels / 640;
+            targetDensity = (float) applicationDisplayMetrics.heightPixels / designSizeDP;
         } else {
-            targetDensity = (float) applicationDisplayMetrics.widthPixels / 360;
+            targetDensity = (float) applicationDisplayMetrics.widthPixels / designSizeDP;
         }
         float targetScaledDensity = targetDensity * (sNoncompatScaledDensity / sNoncompatDensity);
         int targetDensityDpi = (int) (targetDensity * 160);
@@ -85,9 +88,9 @@ public class ScreenAdaptiveUtils {
     /**
      * 关闭适配，恢复之前的 dpi、Density、scaledDensity
      */
-    private static void closeAdaptive(@NonNull Context activity) {
+    private static void closeAdaptive(@NonNull Context activity, @NonNull Context application) {
         DisplayMetrics ResourcesDisplayMetrics = Resources.getSystem().getDisplayMetrics();
-        DisplayMetrics applicationDisplayMetrics = App.getContext().getResources().getDisplayMetrics();
+        DisplayMetrics applicationDisplayMetrics = application.getResources().getDisplayMetrics();
         DisplayMetrics activityDisplayMetrics = activity.getResources().getDisplayMetrics();
 
         applicationDisplayMetrics.scaledDensity = ResourcesDisplayMetrics.scaledDensity;
